924505125c06e1f8ad49c1dd3a2f755a22114d4e
DJG72 <ghoshdebjit72@gmail.com>
2025-11-16 16:13:20 +0530

    Delete backend directory

--- backend/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-const sequelize = new Sequelize({
-  dialect: process.env.DATABASE_DIALECT || 'sqlite',
-  storage: process.env.DATABASE_STORAGE || './database.sqlite',
-  logging: false
-});
-module.exports = sequelize;
--- backend/model/index.js
+++
@@ -1,8 +0,0 @@
-const sequelize = require('../config/database');
-const User = require('./user');
-const Paper = require('./paper');
-const Review = require('./review');
-const Assignment = require('./assignment');
-const Session = require('./session');
-const Certificate = require('./certificate');
-module.exports = { sequelize, User, Paper, Review, Assignment, Session, Certificate };
